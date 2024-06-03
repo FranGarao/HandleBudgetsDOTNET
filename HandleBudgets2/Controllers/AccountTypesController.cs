f0e9bb3c39ac8e0b9016659cd4aebb7a3b0b8ccc
francisco garao <garaofran@gmail.com>
2024-06-03 07:19:17 -0300

    baseCommit

---
+++ HandleBudgets2/Controllers/AccountTypesController.cs
@@ -0,0 +1,22 @@
+﻿using HandleBudgets2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HandleBudgets2.Controllers
+{
+    public class AccountTypesController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(AccountType accountType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(accountType);
+            }
+            return View();
+        }
+    }
+}
---
+++ HandleBudgets2/Models/AccountType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HandleBudgets2.Models
+{
+    public class AccountType
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int xd{ get; set; }
+        public int Order { get; set; }
+    }
+}
